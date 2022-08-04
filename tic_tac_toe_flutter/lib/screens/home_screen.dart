966c806e7527b00c91d6e6d7b11cbd985fc9f82e
Hosein <mrqasemizade@gmail.com>
2022-08-04 08:04:53 +0430

    update Screen

--- tic_tac_toe_flutter/lib/screens/home_screen.dart
+++ tic_tac_toe_flutter/lib/screens/home_screen.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
+  get isTurnO => null;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,49 +15,89 @@ class HomeScreen extends StatelessWidget {
         title: Text('Tic Tac Toe'),
       ),
       backgroundColor: Colors.blueGrey[900],
-      body: Row(
+      body: Column(
         children: [
-          Expanded(
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    'Player O',
-                    style: TextStyle(color: Colors.red, fontSize: 18.0),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    '0',
-                    style: TextStyle(color: Colors.red, fontSize: 18.0),
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Expanded(
-            child: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    'Player X',
-                    style: TextStyle(color: Colors.blue, fontSize: 18.0),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    '0',
-                    style: TextStyle(color: Colors.blue, fontSize: 18.0),
-                  ),
-                ),
-              ],
-            ),
-          ),
+          _getPlayerScoreBoard(),
+          _getGridView(),
         ],
+      ),
+    );
+  }
+
+  Widget _getPlayerScoreBoard() {
+    return Row(
+      children: [
+        Expanded(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  'Player O',
+                  style: TextStyle(color: Colors.red, fontSize: 18.0),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  '0',
+                  style: TextStyle(color: Colors.red, fontSize: 18.0),
+                ),
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  'Player X',
+                  style: TextStyle(color: Colors.blue, fontSize: 18.0),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  '0',
+                  style: TextStyle(color: Colors.blue, fontSize: 18.0),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget getTurn() {
+    return Text(
+      isTurnO ? 'Turn O' : 'TurnX',
+      style: TextStyle(
+        color: Colors.red,
+        fontSize: 20.0,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  }
+
+  Widget _getGridView() {
+    return Expanded(
+      child: GridView.builder(
+        itemCount: 9,
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+        ),
+        itemBuilder: (context, index) {
+          return Container(
+            height: 100.0,
+            width: 100.0,
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.blueGrey),
+            ),
+          );
+        },
       ),
     );
   }
